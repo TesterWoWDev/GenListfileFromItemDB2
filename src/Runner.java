914443c32ae2acd1aa6bccc5be6ae54f5d1384b9
Connor <connorkilbourne@gmail.com>
2020-08-17 14:41:42 -0400

    working, but only prints to consolen

--- src/Runner.java
+++ src/Runner.java
@@ -1,36 +1,110 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 
 public class Runner {
     private static final String delimiter = ",";
     private static final String csvEndSuffix = ".csv";
+    private static HashMap<String, String> fileIDs;
+    private static HashMap<String, String> modelFDID;
+    private static HashMap<String, String> textureFDID;
     private static final String[] tables = new String[12];
     public static void main(String[] args) throws IOException{
+        int count = 0;
         fillTable();
+        startupTables();
+        sortInfoMatRes();
         HashMap<String, String> itemDisplayInfoMaterials = setupDisplayExtraItemsMap(tables[3] + "Sorted" + csvEndSuffix);
-        HashMap<String, String> itemModifiedAppearance = setupItemModMap();
-        HashMap<String, String> itemAppearance = setupItemAppMap();
-        HashMap<String, String> itemModifiedAppearanceReversed = setupItemModReversedMap();
-        HashMap<String, String> itemAppearanceReversed = setupItemAppReversedMap();
-        HashMap<String, String> itemIcon = setupItemMap();
-        HashMap<String, String> itemAppearanceIcon = setupItemAppIconMap();
         try (BufferedReader br = new BufferedReader(new FileReader(tables[2] + csvEndSuffix))) {
             String line;
             br.readLine();//skip header
             while ((line = br.readLine()) != null) {
                 String[] displayRow = line.split(delimiter);
-
-
-
-
-
-
-
+                if(itemDisplayInfoMaterials.get(displayRow[0]) != null){
+                    String displayExtraItemsLine = itemDisplayInfoMaterials.get(displayRow[0]);
+                    String[] itemSplit = displayExtraItemsLine.split(delimiter);
+                    for (String s : itemSplit) {
+                        String[] currItem = s.split("\\.");
+                        String filename;
+                        if(fileIDs.get(textureFDID.get(currItem[1])) == null){//materialID exists, but no path found
+                            if(displayRow[10].equals("0") && !displayRow[11].equals("0") && fileIDs.get(modelFDID.get(displayRow[11])) != null){
+                                filename = fileIDs.get(modelFDID.get(displayRow[11]));
+                            }else if(fileIDs.get(modelFDID.get(displayRow[10])) == null){
+                                filename = "";
+                                count++;
+                            }else{
+                                filename = fileIDs.get(modelFDID.get(displayRow[10]));
+                            }
+                            if(textureFDID.get(currItem[1]) != null)
+                        System.out.println(filename  + textureFDID.get(currItem[1]) + ".blp");
+                        }
+                    }
+                }
             }
         }
+        System.out.println(count);
+    }
+    //sorts itemdisplayinfomaterialres to put all displayIDs in groups for parsing later(cause blizzard cant keep their shit together)
+    private static void sortInfoMatRes() throws IOException {
+        System.out.println("Sorting itemdisplayinfomaterialres...");
+        BufferedReader reader = new BufferedReader(new FileReader(tables[3] + csvEndSuffix));
+        Map<String, List<String>> map = new TreeMap<>();
+        String line;
+        reader.readLine();//skip header
+        while ((line = reader.readLine()) != null) {
+            String key = getField(line);
+            List<String> l = map.computeIfAbsent(key, k -> new LinkedList<>());
+            l.add(line);
+        }
+        reader.close();
+        FileWriter writer = new FileWriter(tables[3] + "Sorted" + csvEndSuffix);
+        for (List<String> list : map.values()) {
+            for (String val : list) {
+                writer.write(val);
+                writer.write("\n");
+            }
+        }
+        writer.close();
+    }
 
+    // extract value you want to sort on(for sorting itemdisplayinfomaterialres)Used in sortInfoMatRes()
+    private static String getField(String line) {
+        return line.split(",")[3];
+    }
+
+    private static HashMap<String, String> setupModelMap() throws IOException
+    {
+        HashMap<String, String> hm = new HashMap<>();
+        BufferedReader br = new BufferedReader(new FileReader(tables[10] + csvEndSuffix));
+        String line;
+        while ( (line = br.readLine()) != null ) {
+            String[] values = line.split(delimiter);
+            hm.put(values[3],values[0]);
+        }
+        br.close();
+        return hm;
+    }
+
+    //setup textureID->FDID map
+    private static HashMap<String, String> setupTextureMap() throws IOException
+    {
+        HashMap<String, String> hm = new HashMap<>();
+        BufferedReader br = new BufferedReader(new FileReader(tables[11] + csvEndSuffix));
+        String line;
+        while ( (line = br.readLine()) != null ) {
+            String[] values = line.split(delimiter);
+            hm.put(values[2],values[0]);
+        }
+        br.close();
+        return hm;
+    }
+    private static void startupTables() throws IOException {
+        System.out.println("Starting Tables...");
+        fileIDs = setupFDIDMap();
+        modelFDID = setupModelMap();
+        textureFDID = setupTextureMap();
     }
     //table and path downloads for files
     private static void fillTable(){
@@ -48,19 +122,6 @@ public class Runner {
         tables[10] = "listfile/modelfiledata";
         tables[11] = "listfile/texturefiledata";
     }
-    private static HashMap<String, String> setupMap(String filename) throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        String line;
-        br.readLine();//skip header
-        while ((line = br.readLine()) != null) {
-            String[] values = line.split(delimiter);
-            hm.put(values[0], line);
-        }
-        br.close();
-        return hm;
-    }
 
     //FDID map creation
     private static HashMap<String, String> setupFDIDMap() throws IOException
@@ -72,90 +133,6 @@ public class Runner {
         while ( (line = br.readLine()) != null ) {
             String[] values = line.split(delim);
             hm.put(values[0],values[1]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //itemModifiedAppearance map creation
-    private static HashMap<String, String> setupItemModMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[4] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[1],values[3]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //itemAppearance map creation
-    private static HashMap<String, String> setupItemAppMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[1] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[0],values[2] + delimiter + values[3]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //setup itemmodifiedappearance map but reversed
-    private static HashMap<String, String> setupItemModReversedMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[4] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[3],values[1]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //setup item appearance map but reversed
-    private static HashMap<String, String> setupItemAppReversedMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[1] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[2],values[0]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //setup item map
-    private static HashMap<String, String> setupItemMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[0] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[0],values[7]);
-        }
-        br.close();
-        return hm;
-    }
-
-    //setup item appearance map for icons
-    private static HashMap<String, String> setupItemAppIconMap() throws IOException
-    {
-        HashMap<String, String> hm = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(tables[1] + csvEndSuffix));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(delimiter);
-            hm.put(values[2],values[3]);
         }
         br.close();
         return hm;
